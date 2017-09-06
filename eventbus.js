c39df08788781830915b867b49dd30bf1d211924
真的是超神哥 <liuxiansenjs@gmail.com>
2017-09-06 20:10:02 +0800

    Update eventbus.js

--- eventbus.js
+++ eventbus.js
@@ -1,22 +1,32 @@
-class EventBus {
-  constructor(_this) {
-    this._this = _this
-    this._listener = []
+(function(global, factory) {
+  if (typeof module !== "undefined" && !!module.exports) {
+    module.exports = factory(global);
+  } else {
+    factory(global, true);
   }
-  emit(eventName, ...args) {
-    try {
-      this._listener.forEach((item) => {
-        if (item.eventName === eventName) {
-          item.fn.call(this._this, ...args)
-        }
-      })
-    } catch (e) {}
-  }
-  on(eventName, fn) {
-    var _env = {
-      eventName,
-      fn,
+})(typeof window !== "undefined" ? window : this, function(global, noGlobal) {
+  class EventBus {
+    constructor(_this) {
+      this._this = _this;
+      this._listener = [];
     }
-    this._listener.push(_env)
+    emit(eventName, ...args) {
+      try {
+        this._listener.forEach(item => {
+          item.eventName === eventName && item.fn.call(this._this, ...args);
+        });
+      } catch (e) {}
+    }
+    on(eventName, fn) {
+      var _env = {
+        eventName,
+        fn
+      };
+      this._listener.push(_env);
+    }
   }
-}
+  if (!!noGlobal) {
+    global.EventBus = EventBus;
+  }
+  return EventBus;
+});
