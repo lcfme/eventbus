f97d280c4b12de191576681e9ed1e9f330e888b5
真的是超神哥 <liuxiansenjs@gmail.com>
2017-09-06 16:58:34 +0800

    Update eventbus.js

--- eventbus.js
+++ eventbus.js
@@ -1,15 +1,18 @@
 class EventBus {
-    constructor() {
-        this._env = {}
-    }
-    emit(eventName, ...args) {
-        try{
-            this._env[eventName].call(null, ...args)
-        } catch(e) {}
-    }
-    on(eventName, callback) {
-        try{
-            this._env[eventName] = callback
-        } catch(e) {}
-    }
+  constructor(_this) {
+    this._this = _this
+    this._listener = []
+  }
+  emit(eventName, ...args) {
+    try {
+      this._listener.forEach((item) => {
+        item[eventName].call(this._this, ...args)
+      })
+    } catch (e) {}
+  }
+  on(eventName, callback) {
+    var _env = {}
+    _env[eventName] = callback
+    this._listener.push(_env)
+  }
 }
